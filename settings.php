92c5b82fb328bd92d08c73c68a59bc8a1accc3e5
Codingiscool21 <nirjan2020@gmail.com>
2026-05-29 10:58:21 +1000

    added settings.php

---
+++ settings.php
@@ -0,0 +1,10 @@
+<?php
+    $host = 'localhost';
+    $user   = "root";
+    $pwd    = "";
+   
+    $db_eoi   = "eoi";
+    $db_jobs  = "jobs";
+    $db_login = "db_login"
+    $db_contributions = "contributions";
+?>
