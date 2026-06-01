37367a28b3ccca0b1bf240f7286174f711fc5187
LachlanColville <S3897401@student.rmit.edu.au>
2026-06-01 19:26:08 +1000

    Fixed a connection error

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 require_once("settings.php");
 
-$conn = mysqli_connect($host, $user, $pwd, $db_login);
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
