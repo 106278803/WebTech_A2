7248bd0dfca2e5aea96be28df6a8e32cecd77094
LachlanColville <S3897401@student.rmit.edu.au>
2026-06-01 17:55:51 +1000

    Merge finish

--- settings.php
+++ settings.php
@@ -8,3 +8,20 @@
     $db_login = "db_login";
     $db_contributions = "contributions";
 ?>
+<?php
+$host = "localhost";
+$user = "root";
+$pwd = "";
+$sql_db = "webtech_a2";
+?>
+
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pwd = "";
+   
+    $db_eoi   = "eoi";
+    $db_jobs  = "jobs";
+    $db_login = "db_login";
+    $db_contributions = "contributions";
+?>
